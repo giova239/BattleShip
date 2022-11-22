5433b820986902be21b62a653ffcf5151de3e296
Giovanni Stevanato <stevanay@gmail.com>
2022-11-22 16:04:37 +0100

    completed ship random placement

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -169,7 +169,9 @@ export class GameComponent implements OnInit {
         }
       }else{
         this.draggingElem.classList.remove("rotated")
-        this.draggingElem.style="";
+        this.draggingElem.style.left= "0px";
+        this.draggingElem.style.top = "0px";
+        this.draggingElem.style.position="static";
       }
 
     }
@@ -184,7 +186,9 @@ export class GameComponent implements OnInit {
       this.dragCoordinates = {x: colCord, y: rowCord};
       var rec = e.target.getBoundingClientRect();
       var offset = document.getElementById("hotbar").getBoundingClientRect();
-      this.draggingElem.style="position:absolute; left:"+ (rec.left - offset.left) +"px; top:"+ (rec.top - offset.top) +"px;";
+      this.draggingElem.style.position="absolute";
+      this.draggingElem.style.left= (rec.left - offset.left) + "px";
+      this.draggingElem.style.top = (rec.top - offset.top) + "px";
     }
   }
 
@@ -209,6 +213,9 @@ export class GameComponent implements OnInit {
 
   checkPlacement(cells){
 
+    if(cells[cells.length-1].x >= this.positioningBoard.length || cells[cells.length-1].y >= this.positioningBoard.length || cells[0].x < 0 || cells[0].y < 0){
+      return false;
+    }
     var from = {x: (cells[0].x)-1, y: (cells[0].y)-1};
     var to = {x: (cells[cells.length-1].x)+1, y: (cells[cells.length-1].y)+1};
     if(from.x < 0) from.x = 0
@@ -299,6 +306,8 @@ export class GameComponent implements OnInit {
 
   randomizePosition(){
 
+    this.positioningBoard = new Array(10).fill(false).map(() => new Array(10).fill(false));
+
     let openCells = new Set<String>();
 
     for(let i = 0; i < 10; i++){
@@ -313,41 +322,100 @@ export class GameComponent implements OnInit {
       Array.from(this.shipsRef.nativeElement.children[0].children as HTMLCollectionOf<HTMLElement>)
     );
 
-    ships.forEach(element => {
-      let rotation = Math.floor(Math.random() * 2);
-      console.log("rotation: " + rotation);
-      let randomMapEntry = (Array.from(openCells.keys()))[Math.floor(Math.random() * openCells.size)];
-      console.log("randomEntry(" + openCells.size + "): " + randomMapEntry[0] + " / " + randomMapEntry[1]);
-      let randomCell = document.getElementById(""+randomMapEntry);
-      console.log(randomCell);
-      let shipType = element.classList[0];
-      let size = 0;
-      if(shipType == "destroyer"){
-        size = 2;
-      }else if(shipType == "cruiser"){
-        size = 3;
-      }else if(shipType == "battleship"){
-        size = 4;
-      }else if(shipType == "carrier"){
-        size = 5;
-      }
-      console.log("size: " + size);
-      //TODO: calculate occupied cells
-      let occupiedCells = [];
-      //TODO: check if position was valid
-      let rec = randomCell.getBoundingClientRect();
-      let offset = document.getElementById("hotbar").getBoundingClientRect();
-      element.style.position="absolute"
-      element.style.left=(rec.left - offset.left)+"px";
-      element.style.top=(rec.top - offset.top)+"px";
-      if(rotation){
-        element.classList.add("rotated")
-      }else{
-        element.classList.remove("rotated")
-      }
-      //TODO: remove occupiedcells from openCells and set true in positioningBoard
-    });
-    
-  }
+    ships.forEach((element, index) => {
+      let guard = false;
+      let randomCell;
+      let rotation;
+      let occupiedCells;
+      let size;
+      let counter = 0;
 
+      while(!guard && counter<100 && openCells.size > 1){
+        rotation = Math.floor(Math.random() * 2);
+        console.log("rotation: " + rotation);
+        let randomMapEntry = (Array.from(openCells.keys()))[Math.floor(Math.random() * openCells.size)];
+        console.log("randomEntry(" + openCells.size + "): " + randomMapEntry[0] + " / " + randomMapEntry[1]);
+        randomCell = document.getElementById(""+randomMapEntry);
+        console.log(randomCell);
+        let shipType = element.classList[0];
+        size = 0;
+        if(shipType == "destroyer"){
+          size = 2;
+        }else if(shipType == "cruiser"){
+          size = 3;
+        }else if(shipType == "battleship"){
+          size = 4;
+        }else if(shipType == "carrier"){
+          size = 5;
+        }
+        console.log("size: " + size);
+        occupiedCells = [];
+        if(rotation == 1){
+          for(let i = 0; i < size; i++){
+            occupiedCells.push({x: Number(randomMapEntry[1]), y: Number(randomMapEntry[0])+i})
+          }
+        }else{
+          for(let i = 0; i < size; i++){
+            occupiedCells.push({x: Number(randomMapEntry[1])+i, y: Number(randomMapEntry[0])})
+          }
+        }
+        console.log(occupiedCells);
+        guard = this.checkPlacement(occupiedCells);
+        console.log(guard);
+        counter++;
+        console.log(counter);
+      }
+
+      if(guard){
+        occupiedCells.forEach(e => {
+          this.positioningBoard[e.y][e.x] = true;
+        })
+        if(size == 2){
+          this.ships.destroyers.cells[index-this.ships.cruisers.cells.length-this.ships.battleships.cells.length-this.ships.carrier.cells.length] = occupiedCells;
+        }else if (size == 3){
+          this.ships.cruisers.cells[index-this.ships.battleships.cells.length-this.ships.carrier.cells.length] = occupiedCells;
+        }else if(size == 4){
+          this.ships.battleships.cells[index-this.ships.carrier.cells.length] = occupiedCells;
+        }else if(size == 5){
+          this.ships.carrier.cells[index] = occupiedCells;
+        }
+        var from = {x: (occupiedCells[0].x)-1, y: (occupiedCells[0].y)-1};
+        var to = {x: (occupiedCells[occupiedCells.length-1].x)+1, y: (occupiedCells[occupiedCells.length-1].y)+1};
+        if(from.x < 0) from.x = 0
+        if(from.y < 0) from.y = 0
+        if(to.x > this.positioningBoard.length-1) to.x = this.positioningBoard.length-1
+        if(to.y > this.positioningBoard.length-1) to.y = this.positioningBoard.length-1
+        for(let i = from.y; i <= to.y; i++){
+          for(let j = from.x; j <= to.x; j++){
+            openCells.delete(i.toString()+j.toString())
+          }
+        }
+        console.log(openCells);
+        let rec = randomCell.getBoundingClientRect();
+        let offset = document.getElementById("hotbar").getBoundingClientRect();
+        element.style.position="absolute";
+        element.style.left=(rec.left - offset.left)+"px";
+        element.style.top=(rec.top - offset.top)+"px";
+        if(rotation){
+          element.classList.add("rotated")
+        }else{
+          element.classList.remove("rotated")
+        }
+      }else{
+        if(size == 2){
+          this.ships.destroyers.cells[index-this.ships.cruisers.cells.length-this.ships.battleships.cells.length-this.ships.carrier.cells.length] = occupiedCells;
+        }else if (size == 3){
+          this.ships.cruisers.cells[index-this.ships.battleships.cells.length-this.ships.carrier.cells.length] = occupiedCells;
+        }else if(size == 4){
+          this.ships.battleships.cells[index-this.ships.carrier.cells.length] = occupiedCells;
+        }else if(size == 5){
+          this.ships.carrier.cells[index] = occupiedCells;
+        }
+        element.classList.remove("rotated")
+        element.style.left= "0px";
+        element.style.top = "0px";
+        element.style.position="static";
+      }
+    });
+  }
 }
