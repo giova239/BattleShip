4e258a37a14b728c8fb2e8ccc0db62caaf381044
Giovanni Stevanato <stevanay@gmail.com>
2023-01-03 19:28:37 +0100

    added profile page with win rate and hit rate

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -12,7 +12,9 @@ export class ProfileComponent implements OnInit {
   constructor(public us: UserHttpService, public route: ActivatedRoute) { }
 
   public userID;
-  public user;
+  public user: any;
+  public friendState = 0;
+  //0 -> not-friends; 1 -> friend-request-sent; 2 -> friends; 3 -> yourProfile
 
   ngOnInit(): void {
 
@@ -20,10 +22,37 @@ export class ProfileComponent implements OnInit {
 
       this.userID = params['userID'];
 
-      this.us.get_user_by_id(this.userID).subscribe(u => this.user = u)
+      this.us.get_user_by_id(this.userID).subscribe(u => {
+        this.user = u;
+        if(this.user.friends.includes(this.us.get_id())){
+          this.friendState = 2
+        }else if(this.user.pendingRequests.includes(this.us.get_id())){
+          this.friendState = 1
+        }else if(this.us.get_id() == this.userID){
+          this.friendState = 3
+        }
+        console.log(this.friendState);
+        
+      })
 
     });
 
+  }
+
+  friendButtonClicked(){
+
+    if(this.friendState == 0){
+      //addFriend
+      this.us.post_friends(this.userID).subscribe( resp => {
+        this.friendState=1;
+      })
+    }else if(this.friendState == 1){
+      //removeRequest
+      this.friendState = 0;
+    }else if(this.friendState == 2){
+      //removeFriend
+      this.friendState = 0;
+    }
   }
 
 }
